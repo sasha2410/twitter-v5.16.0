df0921a2177d23e057b3e28bddac1950ef7eb053
Alex Shulyndin <sasha2410@mail.ru>
2020-03-17 18:23:18 +0400

    Initial Commit

---
+++ examples/Update.md
@@ -0,0 +1,66 @@
+# Update
+
+These examples assume you have a configured Twitter REST `client`.
+Instructions on how to configure a client can be found in
+[examples/Configuration.md][cfg].
+
+[cfg]: https://github.com/sferik/twitter/blob/master/examples/Configuration.md
+
+If the authenticated user has granted read/write permission to your
+application, you may tweet as them.
+
+```ruby
+client.update("I'm tweeting with @gem!")
+```
+
+Post an update in reply to another tweet.
+
+```ruby
+client.update("I'm tweeting with @gem!", :in_reply_to_status_id => 402712877960019968)
+```
+
+Post an update with precise coordinates.
+
+```ruby
+client.update("I'm tweeting with @gem!", :lat => 37.7821120598956, :long => -122.400612831116, :display_coordinates => true)
+```
+
+Post an update from a specific place. Place IDs can be retrieved using the
+[`#reverse_geocode`][reverse_geocode] method.
+
+[reverse_geocode]: http://rdoc.info/gems/twitter/Twitter/REST/API/PlacesAndGeo#reverse_geocode-instance_method
+
+```ruby
+client.update("I'm tweeting with @gem!", :place_id => "df51dec6f4ee2b2c")
+```
+
+Post an update with an image.
+
+```ruby
+client.update_with_media("I'm tweeting with @gem!", File.new("/path/to/media.png"))
+```
+
+Post an update with a possibly-sensitive image.
+
+```ruby
+client.update_with_media("I'm tweeting with @gem!", File.new("/path/to/sensitive-media.png"), :possibly_sensitive => true)
+```
+
+Post an update with multiple images.
+
+This will return media IDs, which you can pass into the media_ids parameter (as a comma-separated string) of the update method.
+```ruby
+media_ids = %w(/path/to/media1.png /path/to/media2.png).map do |filename|
+  Thread.new do
+    client.upload(File.new(filename))
+  end
+end.map(&:value)
+
+client.update("I'm tweeting with @gem!", :media_ids => media_ids.join(','))
+```
+
+For more information, see the documentation for the [`#update`][update] and
+[`#update_with_media`][update_with_media] methods.
+
+[update]: http://rdoc.info/gems/twitter/Twitter/REST/Tweets#update-instance_method
+[update_with_media]: http://rdoc.info/gems/twitter/Twitter/REST/Tweets#update_with_media-instance_method
---
+++ spec/twitter/rest/client_spec.rb
@@ -0,0 +1,168 @@
+require 'helper'
+
+describe Twitter::REST::Client do
+  before do
+    @client = Twitter::REST::Client.new(:consumer_key => 'CK', :consumer_secret => 'CS', :access_token => 'AT', :access_token_secret => 'AS')
+  end
+
+  describe '.new' do
+    context 'when invalid credentials are provided' do
+      it 'raises a ConfigurationError exception' do
+        expect { Twitter::REST::Client.new(:consumer_key => [12_345, 54_321]) }.to raise_exception(Twitter::Error::ConfigurationError)
+      end
+    end
+    context 'when no credentials are provided' do
+      it 'does not raise an exception' do
+        expect { Twitter::REST::Client.new }.not_to raise_error
+      end
+    end
+  end
+
+  describe '#get' do
+    before do
+      stub_get('/path')
+    end
+    it 'performs an HTTP GET' do
+      capture_warning do
+        @client.get('/path')
+      end
+      expect(a_get('/path')).to have_been_made
+    end
+    it 'outputs a warning' do
+      warning = capture_warning do
+        @client.get('/path')
+      end
+      expect(warning).to match(/\[DEPRECATION\] Twitter::REST::Client#get is deprecated\. Use Twitter::REST::Request#perform instead\.$/)
+    end
+  end
+
+  describe '#post' do
+    before do
+      stub_post('/path')
+    end
+    it 'performs an HTTP GET' do
+      capture_warning do
+        @client.post('/path')
+      end
+      expect(a_post('/path')).to have_been_made
+    end
+    it 'outputs a warning' do
+      warning = capture_warning do
+        @client.post('/path')
+      end
+      expect(warning).to match(/\[DEPRECATION\] Twitter::REST::Client#post is deprecated\. Use Twitter::REST::Request#perform instead\.$/)
+    end
+  end
+
+  describe '.credentials?' do
+    it 'returns true if only bearer_token is supplied' do
+      client = Twitter::REST::Client.new(:bearer_token => 'BT')
+      expect(client.credentials?).to be true
+    end
+    it 'returns true if all OAuth credentials are present' do
+      client = Twitter::REST::Client.new(:consumer_key => 'CK', :consumer_secret => 'CS', :access_token => 'AT', :access_token_secret => 'AS')
+      expect(client.credentials?).to be true
+    end
+    it 'returns false if any credentials are missing' do
+      client = Twitter::REST::Client.new(:consumer_key => 'CK', :consumer_secret => 'CS', :access_token => 'AT')
+      expect(client.credentials?).to be false
+    end
+  end
+
+  it 'does not cache the screen name across clients' do
+    stub_get('/1.1/account/verify_credentials.json').to_return(:body => fixture('sferik.json'), :headers => {:content_type => 'application/json; charset=utf-8'})
+    user1 = Twitter::REST::Client.new(:consumer_key => 'CK', :consumer_secret => 'CS', :access_token => 'AT', :access_token_secret => 'AS').current_user
+    stub_get('/1.1/account/verify_credentials.json').to_return(:body => fixture('pengwynn.json'), :headers => {:content_type => 'application/json; charset=utf-8'})
+    user2 = Twitter::REST::Client.new(:consumer_key => 'CK', :consumer_secret => 'CS', :access_token => 'AT', :access_token_secret => 'AS').current_user
+    expect(user1).not_to eq(user2)
+  end
+
+  describe '#user_token?' do
+    it 'returns true if the user token/secret are present' do
+      client = Twitter::REST::Client.new(:consumer_key => 'CK', :consumer_secret => 'CS', :access_token => 'AT', :access_token_secret => 'AS')
+      expect(client.user_token?).to be true
+    end
+    it 'returns false if the user token/secret are not completely present' do
+      client = Twitter::REST::Client.new(:consumer_key => 'CK', :consumer_secret => 'CS', :access_token => 'AT')
+      expect(client.user_token?).to be false
+    end
+  end
+
+  describe '#bearer_token?' do
+    it 'returns true if the app token is present' do
+      client = Twitter::REST::Client.new(:consumer_key => 'CK', :consumer_secret => 'CS', :bearer_token => 'BT')
+      expect(client.bearer_token?).to be true
+    end
+    it 'returns false if the bearer_token is not present' do
+      client = Twitter::REST::Client.new(:consumer_key => 'CK', :consumer_secret => 'CS')
+      expect(client.bearer_token?).to be false
+    end
+  end
+
+  describe '#connection_options=' do
+    it 'sets connection options' do
+      capture_warning do
+        @client.connection_options = 'connection options'
+      end
+      expect(@client.connection_options).to eq('connection options')
+    end
+    it 'outputs a warning' do
+      warning = capture_warning do
+        @client.connection_options = nil
+      end
+      expect(warning).to match(/\[DEPRECATION\] Twitter::REST::Client#connection_options= is deprecated and will be removed\.$/)
+    end
+  end
+
+  describe '#connection_options' do
+    it 'returns the connection options hash with proxy and user_agent' do
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = 'CK'
+        config.consumer_secret     = 'CS'
+        config.access_token        = 'AT'
+        config.access_token_secret = 'ATS'
+        config.proxy               = 'http://localhost:99'
+        config.user_agent          = 'My Twitter Ruby Gem'
+      end
+      expect(client.connection_options[:proxy]).to eql('http://localhost:99')
+      expect(client.connection_options[:headers][:user_agent]).to eql('My Twitter Ruby Gem')
+    end
+  end
+
+  describe '#middleware=' do
+    it 'sets middleware' do
+      capture_warning do
+        @client.middleware = 'middleware'
+      end
+      expect(@client.middleware).to eq 'middleware'
+    end
+    it 'outputs a warning' do
+      warning = capture_warning do
+        @client.middleware = nil
+      end
+      expect(warning).to match(/\[DEPRECATION\] Twitter::REST::Client#middleware= is deprecated and will be removed\.$/)
+    end
+  end
+
+  describe '#credentials?' do
+    it 'returns true if all credentials are present' do
+      client = Twitter::REST::Client.new(:consumer_key => 'CK', :consumer_secret => 'CS', :access_token => 'AT', :access_token_secret => 'AS')
+      expect(client.credentials?).to be true
+    end
+    it 'returns false if any credentials are missing' do
+      client = Twitter::REST::Client.new(:consumer_key => 'CK', :consumer_secret => 'CS', :access_token => 'AT')
+      expect(client.credentials?).to be false
+    end
+  end
+
+  describe '#connection' do
+    it 'looks like Faraday connection' do
+      expect(@client.send(:connection)).to respond_to(:run_request)
+    end
+    it 'memoizes the connection' do
+      c1 = @client.send(:connection)
+      c2 = @client.send(:connection)
+      expect(c1.object_id).to eq(c2.object_id)
+    end
+  end
+end
